07ea223cd8f0b774a46e55a0f4932331b8449e90
Haider <haideralidevnull@gmail.com>
2026-06-26 07:04:24 +0000

    clean up package__init__

--- fastapi_interactions/__init__.py
+++ fastapi_interactions/__init__.py
@@ -1,16 +1,8 @@
-# from .bot import Bot
-# from .commands import CommandRouter, option
-# from . import responses
-
-# __all__ = [
-#     'Bot',
-#     'CommandRouter',
-#     'option'
-# ]
-
 from .bot import Bot
-from . import commands
-from . import responses
-from . import models
+from .commands import CommandRouter, option
 
-__all__ = ["Bot", "commands", "responses", "models"]
+__all__ = [
+    'Bot',
+    'CommandRouter',
+    'option'
+]
